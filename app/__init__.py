32353bfeb169a80d6137a15e807b0c6690025c56
Kanyon Edvall <kanyon@life-foundry.com>
2020-10-26 16:28:09 -0500

    Chapter 3 - Web Forms

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 # Load config
-flask_config = os.getenv('APP_SETTINGS')
+flask_config = os.getenv('APP_CONFIG')
 if flask_config is None:
     # print("- Warning: APP_SETTINGS not found")
     flask_config = 'DevelopmentConfig'
@@ -12,6 +12,6 @@ print(f"Config: {flask_config}")
 
 # Create Flask instance
 flask_app = Flask(__name__)
-flask_app.config.from_object('app.app_config.' + flask_config)
+flask_app.config.from_object('app.config.' + flask_config)
 
 from app import routes
