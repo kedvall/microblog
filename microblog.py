10fa38e9602d21baa70b5260e007a5e3e03cd183
Kanyon Edvall <kanyon@life-foundry.com>
2020-10-28 01:49:43 -0500

    Chapter 4 - Database

--- microblog.py
+++ microblog.py
@@ -1,4 +1,12 @@
-from app import flask_app
+from app import flask_app, db
+from app.models import User, Post
+
+
+@flask_app.shell_context_processor
+def make_shell_context():
+    print("Added shell context")
+    return {'db': db, 'User': User, 'Post': Post}
+
 
 if __name__ == "__main__":
     flask_app.run()
