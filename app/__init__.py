18ea6fbcada1844ae47c55b955ebe38b6b4b2e4a
Kanyon Edvall <kanyon@life-foundry.com>
2020-10-29 14:12:01 -0500

    Chapter 7 - Error Handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,6 +11,7 @@ flask_config = os.getenv('APP_CONFIG')
 if flask_config is None:
     # print("- Warning: APP_SETTINGS not found")
     flask_config = 'DevelopmentConfig'
+    # flask_config = 'ProductionConfig'
 
 print(f"Name: {__name__}")
 print(f"Config: {flask_config}")
@@ -25,4 +28,31 @@ migrate = Migrate(flask_app, db)
 login = LoginManager(flask_app)
 login.login_view = 'login'
 
-from app import routes, models
+# Loggin setup
+if not flask_app.debug:
+    if flask_app.config['MAIL_SERVER']:
+        auth = None
+        if flask_app.config['MAIL_USERNAME'] or flask_app.config['MAIL_PASSWORD']:
+            auth = (flask_app.config['MAIL_USERNAME'], flask_app.config['MAIL_PASSWORD'])
+        secure = None
+        if flask_app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(flask_app.config['MAIL_SERVER'], flask_app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + flask_app.config['MAIL_SERVER'],
+            toaddrs=flask_app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        flask_app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d'))
+    file_handler.setLevel(logging.INFO)
+    flask_app.logger.addHandler(file_handler)
+
+    flask_app.logger.setLevel(logging.INFO)
+    flask_app.logger.info('Microblog startup')
+
+from app import routes, models, errors
--- app/config.py
+++ app/config.py
@@ -3,13 +3,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class AppConfig(object):
+    # Flask config
     DEBUG = False
     TETING = False
     CSRF_ENABLED = False
     SECRET_KEY = "MY-SUPER-SECRET-KEY"
-
+    # Database config
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'microblog_app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Email notification config
+    MAIL_SERVER = 'localhost'
+    MAIL_PORT = 8025
+    MAIL_USE_TLS = None
+    MAIL_USERNAME = None
+    MAIL_PASSWORD = None
+    ADMINS = ['my_application@gmx.com']
 
 
 class ProductionConfig(AppConfig):
